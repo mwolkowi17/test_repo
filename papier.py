4a3e36c3fedcf2ecbcfb75e7c267a7460dde9b47
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2018-10-27 22:37:34 +0200

    poczatek

---
+++ papier.py
@@ -0,0 +1,8 @@
+a=input('podaj papier kamien nożyce')
+b=input('podaj papier kamien nożyce')
+if a==('kamien') and b==('nożyce'):
+    print('a wygrales')
+if a==('papier') and b==('nożyce'):
+    print('b wygrałeś')
+else:
+    print('i tak dalej')
